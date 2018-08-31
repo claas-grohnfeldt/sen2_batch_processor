76a46725c8ac83c6541db703c9997d67d20865e0
claasg <claas.grohnfeldt@gmail.com>
2018-08-31 15:51:02 +0200

    updated

--- setup.sh
+++ setup.sh
@@ -6,9 +6,12 @@ conda create -n sen2_batch_processor python=3.6
 # activate conda environment
 source activate sen2_batch_processor
 
-# install python packages listed in the file 'requirements.txt'
-while read requirement; do 
-    # note: the parameter "-c conda-forge" can be rem
-	conda install --yes -c conda-forge $requirement
-done < requirements.txt
+# # install python packages listed in the file 'requirements.txt'
+# while read requirement; do 
+#     # note: the parameter "-c conda-forge" can be removed 
+#     #       as soon as gdal version >= 2.3.1 becomes
+#     #       available in the default anaconda channel
+#     #       (check status on https://anaconda.org/anaconda/gdal)
+# 	conda install --yes -c conda-forge $requirement
+# done < requirements.txt
 
