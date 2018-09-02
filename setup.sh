e2fe9fc57fa6f764633988cdad60af6bd27fa227
claasg <claas.grohnfeldt@gmail.com>
2018-09-02 13:58:09 +0200

    minor update

--- setup.sh
+++ setup.sh
@@ -1,20 +1,18 @@
 #!/usr/bin/env bash
 
-# create an anaconda environment for Python 3.6
-conda create -n sen2_batch_processor \ #  
-             -y \ #                       Do not ask for confirmation.
-             python=3.6 \
+# Create an anaconda environment for Python 3.6
+# named "sen2_batch_processor" without asking for 
+# confirmation. Package version takes precedence 
+# over channel prior. Lastly, update dependencies.
+conda create -n sen2_batch_processor -y python=3.6 \
+             --no-channel-priority --update-deps
 
+# Add conda channel conda-forge, which - at the time of
+# development - was ahead of the default channels by
+# means of gdal version 2.3.1 (which is required by
+# the Sentinel-2 processing tools)
+conda config --add channels conda-forge
+conda install --name sen2_batch_processor -y --file requirements.txt
 
 # activate conda environment
 source activate sen2_batch_processor
-
-# # install python packages listed in the file 'requirements.txt'
-# while read requirement; do 
-#     # note: the parameter "-c conda-forge" can be removed 
-#     #       as soon as gdal version >= 2.3.1 becomes
-#     #       available in the default anaconda channel
-#     #       (check status on https://anaconda.org/anaconda/gdal)
-# 	conda install --yes -c conda-forge $requirement
-# done < requirements.txt
-
