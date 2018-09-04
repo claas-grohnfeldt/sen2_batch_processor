0ee3ba4e697a0b896e9a72b086b725d0c60db7ad
claasg <claas.grohnfeldt@gmail.com>
2018-09-04 12:42:23 +0200

    implementation of sen2cor installation procedure completed.

--- install.sh
+++ install.sh
@@ -108,10 +108,82 @@ for MACHINE in "scihub.copernicus.eu" "sso.eoc.dlr.de"; do
 done
 chmod 400 ${HOME}/.netrc
 
+
 echo
 echo "> +-------------------------------------"
-echo "  | Setting up conda environment and"
+echo "  | Setting up sen2cor"
+echo "  +-------------------------------------"
+INSTALL_SEN2COR=true
+# Check if there exists a local installation of sen2cor in this project directory. 
+if [ -f "${PATH_TARGET_DIR_SEN2COR}/bin/L2A_process" ]; then
+	echo "  File '${PATH_TARGET_DIR_SEN2COR}/bin/L2A_process' already exists.";
+	if [ "$(bash ${PATH_TARGET_DIR_SEN2COR}/bin/L2A_process -h | grep 'Sentinel-2 Level 2A Processor')" ] ; then 
+		echo "  Program seems to be OK.";
+		echo
+		read -p "> Would you like to use this existing installation? (Y/N) [Y]: " USE_EXISTING_SEN2COR
+		if ! [[ $USE_EXISTING_SEN2COR == [nN] || $USE_EXISTING_SEN2COR == [nN][oO] ]]; then
+			INSTALL_SEN2COR=false
+			PATH_TARGET_FILE_L2A_Process=${PATH_TARGET_DIR_SEN2COR}/bin/L2A_process
+		fi
+	else
+		echo "  Warning: Something is wrong with that existing program (L2A_process)."
+	fi
+fi
+if $INSTALL_SEN2COR; then
+	echo "> Do you have recent version of sen2cor (version >= 2.5.5) installed on your"
+	echo "  machine which you would like to use in this project? (If not, sen2cor will"
+	printf "  be downloaded and installed locally in this project folder? (Y/N) [N]: "
+	read sen2cor_installed 
+	
+	if [[ $sen2cor_installed == [yY] || $sen2cor_installed == [yY][eE][sS] ]]; then
+		echo "> Please enter the full path of the file 'L2A_Process' (by default to be found"
+		read -p "  <path_to_sen2cor_dir>/bin/L2A_Process): " PATH_TARGET_FILE_L2A_Process
+		if [ -f $PATH_TARGET_FILE_L2A_Process ] && [ "$(bash $PATH_TARGET_FILE_L2A_Process -h | grep 'Sentinel-2 Level 2A Processor')" ]; then
+			echo "  Path and program (L2A_Process) seem to be OK."
+			echo "  Creating symbolic link to the following local project directory:"
+			echo "  ${PATH_TARGET_DIR_SEN2COR}/bin/L2A_Process"
+			mkdir -p ${PATH_TARGET_DIR_SEN2COR}/bin
+			ln -s $PATH_TARGET_FILE_L2A_Process ${PATH_TARGET_DIR_SEN2COR}/bin/L2A_Process
+			echo "  Succeeded."
+		else
+			echo "> Warning: There is something wrong with this path or the program (L2A_Process) itself.'"
+			echo
+			echo "> Downloading and installing sen2cor now."
+			wget 'http://step.esa.int/thirdparties/sen2cor/2.5.5/Sen2Cor-02.05.05-Linux64.run' -P $PATH_DIR_TMP
+			bash "${PATH_DIR_TMP}/Sen2Cor-02.05.05-Linux64.run" --target "${PATH_TARGET_DIR_SEN2COR}"
+			PATH_TARGET_FILE_L2A_Process="${PATH_TARGET_DIR_SEN2COR}/bin/L2A_Process"
+		fi
+	else
+		echo "  Alright. Let's download and install it now."
+		echo 
+		echo "> Downloading and installing sen2cor .."
+		wget 'http://step.esa.int/thirdparties/sen2cor/2.5.5/Sen2Cor-02.05.05-Linux64.run' -P $PATH_DIR_TMP
+		bash "${PATH_DIR_TMP}/Sen2Cor-02.05.05-Linux64.run" --target "${PATH_TARGET_DIR_SEN2COR}"
+		PATH_TARGET_FILE_L2A_Process="${PATH_TARGET_DIR_SEN2COR}/bin/L2A_Process"
+	fi
+	echo
+	echo 'Note:'
+	echo 'Sen2Cor configuration options and software documentation are available here:'
+	echo 'http://step.esa.int/thirdparties/sen2cor/2.5.5/docs/S2-PDGS-MPC-L2A-SUM-V2.5.5_V2.pdf'
+	echo
+fi
+# TODO: PATH_TARGET_FILE_L2A_Process Needed? If so, make variable available for later use (export?)
+
+echo
+echo "> +-------------------------------------"
+echo "  | Setting up a conda environment and"
 echo "  | installing required python packages"
+echo "  | in that environment."
+echo "  +-------------------------------------"
+# download code-de-tools
+# TODO
+
+
+echo
+echo "> +-------------------------------------"
+echo "  | Setting up a conda environment and"
+echo "  | installing required python packages"
+echo "  | in that environment."
 echo "  +-------------------------------------"
 # Create an anaconda environment for Python 3.6
 # named "sen2_batch_processor" without asking for 
@@ -128,48 +200,11 @@ conda config --add channels conda-forge
 conda install --name sen2_batch_processor -y --file requirements.txt
 
 
-
-echo
-echo "> +-------------------------------------"
-echo "  | Setting up sen2cor"
-echo "  +-------------------------------------"
-read -p "> Do you have sen2cor (version >= 2.5.5) already installed and would like to the working directory instead of downloading and installing it? (Y/N) [N]: " sen2cor_installed 
-if [[ $sen2cor_installed == [yY] || $sen2cor_installed == [yY][eE][sS] ]]; then
-	read -p "> Please enter the full path of the file 'L2A_Process' (by default to be found in <path_to_sen2cor_dir>/bin/L2A_Process)" PATH_TARGET_FILE_L2A_Process
-	if [ -f PATH_TARGET_FILE_L2A_Process ] && [[ ${PATH_TARGET_FILE_L2A_Process} = *"/L2A_Process" ]]; then
-		echo "  path OK."
-		export PATH_TARGET_FILE_L2A_Process=${PATH_TARGET_FILE_L2A_Process}
-	else
-		echo "> Warning: path should end with '/L2A_Process'"
-		echo
-		echo "> Downloading and installing sen2cor .."
-		wget 'http://step.esa.int/thirdparties/sen2cor/2.5.5/Sen2Cor-02.05.05-Linux64.run' -P $PATH_DIR_TMP
-		bash "${PATH_DIR_TMP}/Sen2Cor-02.05.05-Linux64.run" --target "${PATH_TARGET_DIR_SEN2COR}"
-		export PATH_TARGET_FILE_L2A_Process="${PATH_TARGET_DIR_SEN2COR}/bin/L2A_Process"
-	fi
-else
-	echo "  Alright. Let's download and install it now."
-	echo 
-	echo "> Downloading and installing sen2cor .."
-	wget 'http://step.esa.int/thirdparties/sen2cor/2.5.5/Sen2Cor-02.05.05-Linux64.run' -P $PATH_DIR_TMP
-	bash "${PATH_DIR_TMP}/Sen2Cor-02.05.05-Linux64.run" --target "${PATH_TARGET_DIR_SEN2COR}"
-	export PATH_TARGET_FILE_L2A_Process="${PATH_TARGET_DIR_SEN2COR}/bin/L2A_Process"
-fi
-
-echo
-echo 'Note:'
-echo 'Sen2Cor configuration options and software documentation are available here:'
-echo 'http://step.esa.int/thirdparties/sen2cor/2.5.5/docs/S2-PDGS-MPC-L2A-SUM-V2.5.5_V2.pdf'
-echo
-
-# download code-de-tools
-# TODO
-
 # download superres code
 # TODO
 
 
-#rm -r $PATH_DIR_TMP
+rm -r $PATH_DIR_TMP
 
 # activate conda environment
 source activate sen2_batch_processor
