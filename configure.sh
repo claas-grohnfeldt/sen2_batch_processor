1bbcff2469b50a0b55cbff4002f87e91dd4caaa9
claasg <claas.grohnfeldt@gmail.com>
2018-09-04 23:15:09 +0200

    added configuration file. Currently, onle paths and the name of the conda environment are set. May be extended later

---
+++ configure.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+PATH_TARGET_DIR_MAIN=$(pwd)
+PATH_DIR_TMP="${PATH_TARGET_DIR_MAIN}/tmp"
+PATH_TARGET_DIR_SEN2COR="${PATH_TARGET_DIR_MAIN}/src/thirdparties/sen2cor_woanders"
+PATH_TARGET_DIR_CODE_DE_TOOLS="${PATH_TARGET_DIR_MAIN}/src/thirdparties/code-de-tools_elsewhere"
+
+PATH_FILE_NETRC="${PATH_TARGET_DIR_MAIN}/.netrc"
+
+NAME_CONDA_ENVIRONMENT="sen2_batch_processor"
