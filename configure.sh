694372a27835e7b3623fa6117001172b7534cf2c
claasg <claas.grohnfeldt@gmail.com>
2018-09-05 15:32:58 +0200

    added sen2cor settings

--- configure.sh
+++ configure.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-PATH_TARGET_DIR_MAIN=$(pwd)
-PATH_DIR_TMP="${PATH_TARGET_DIR_MAIN}/tmp"
-PATH_TARGET_DIR_CODE_DE_TOOLS="${PATH_TARGET_DIR_MAIN}/src/thirdparties/code-de-tools"
-PATH_DIR_DATA="${PATH_TARGET_DIR_MAIN}/data"
-PATH_FILE_NETRC="${PATH_TARGET_DIR_MAIN}/.netrc"
+PATH_DIR_MAIN=$(pwd)
+PATH_DIR_TMP="${PATH_DIR_MAIN}/tmp"
+PATH_DIR_CODE_DE_TOOLS="${PATH_DIR_MAIN}/src/thirdparties/code-de-tools"
+PATH_DIR_DATA="${PATH_DIR_MAIN}/data"
+PATH_FILE_NETRC="${PATH_DIR_MAIN}/.netrc"
 
 NAME_CONDA_ENVIRONMENT="sen2_batch_processor"
 
@@ -12,4 +12,4 @@ SOURCE_S2_TILING_GRID="https://sentinel.esa.int/documents/247904/1955685/S2A_OPE
 PATH_FILE_S2_TILING_GRID="$PATH_DIR_DATA/Sentinel2_global_tiling_grid.kml"
 
 SOURCE_SEN2COR="http://step.esa.int/thirdparties/sen2cor/2.5.5/Sen2Cor-02.05.05-Linux64.run"
-PATH_TARGET_DIR_SEN2COR="${PATH_TARGET_DIR_MAIN}/src/thirdparties/sen2cor"
+PATH_DIR_SEN2COR="${PATH_DIR_MAIN}/src/thirdparties/sen2cor"
