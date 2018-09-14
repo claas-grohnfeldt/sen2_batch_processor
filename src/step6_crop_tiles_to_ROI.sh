1cbae39dded9ec0e600dbbec3debb42534800088
claasg <claas.grohnfeldt@dlr.de>
2018-09-14 22:08:13 +0200

    adapted names to global convention scheme

--- src/step6_crop_tiles_to_ROI.sh
+++ src/step6_crop_tiles_to_ROI.sh
@@ -2,36 +2,36 @@
 
 format_file_out="ENVI"
 
-path_dir_input="/mnt/ssd2/Projects/S2_superresolution/data/sen2_jio/LCZ42_204371_Munich/winter/tiles"
+PATH_DIR_INPUT="/mnt/ssd2/Projects/S2_superresolution/data/sen2_jio/LCZ42_204371_Munich/winter/tiles"
 
 # --------------------
-#tile_file_input_list=${path_dir_input}/*.SAFE.zip
-tile_file_input_list=${path_dir_input}/*_MSIL1C_*.SAFE/MTD_MSIL1C.xml
-#tile_file_input_list=${path_dir_input}/*_MSIL2A_*.SAFE/MTD_MSIL2A.xml
-path_ROI="${path_dir_input}/../../ROI.csv"
-path_dir_out=${path_dir_input}/tiles_cropped_to_ROI_0
-mkdir -p $path_dir_out
+#TILE_FILE_INPUT_LIST=${PATH_DIR_INPUT}/*.SAFE.zip
+TILE_FILE_INPUT_LIST=${PATH_DIR_INPUT}/*_MSIL1C_*.SAFE/MTD_MSIL1C.xml
+#TILE_FILE_INPUT_LIST=${PATH_DIR_INPUT}/*_MSIL2A_*.SAFE/MTD_MSIL2A.xml
+PATH_ROI="${PATH_DIR_INPUT}/../../ROI.csv"
+PATH_DIR_OUT=${PATH_DIR_INPUT}/tiles_cropped_to_ROI_0
+mkdir -p $PATH_DIR_OUT
 
-echo "path_dir_input = $path_dir_input"
-echo "tile_file_input_list = $tile_file_input_list"
-echo "path_ROI = $path_ROI"
-echo "path_dir_out = $path_dir_out"
+echo "PATH_DIR_INPUT = $PATH_DIR_INPUT"
+echo "TILE_FILE_INPUT_LIST = $TILE_FILE_INPUT_LIST"
+echo "PATH_ROI = $PATH_ROI"
+echo "PATH_DIR_OUT = $PATH_DIR_OUT"
 
-for path_file_input in $tile_file_input_list; do
-    echo "path_file_input = $path_file_input"
-    roi_lon_lat=$(cat $path_ROI)
-    echo "roi_lon_lat = $roi_lon_lat"
- 	#fname_file_input=$(basename -- "$path_file_input")
-    fname_file_input=$(echo $path_file_input | sed 's/.SAFE.*/\.SAFE/' | xargs basename)
- 	fname_file_out="${fname_file_input%%.*}_cropped"
- 	#roi_lon_lat="4.129657,1.668,79.123,80.598"
+for PATH_FILE_INPUT in $TILE_FILE_INPUT_LIST; do
+    echo "PATH_FILE_INPUT = $PATH_FILE_INPUT"
+    ROI_LON_LAT=$(cat $PATH_ROI)
+    echo "ROI_LON_LAT = $ROI_LON_LAT"
+ 	#fname_file_input=$(basename -- "$PATH_FILE_INPUT")
+    fname_file_input=$(echo $PATH_FILE_INPUT | sed 's/.SAFE.*/\.SAFE/' | xargs basename)
+ 	FNAME_FILE_OUT="${fname_file_input%%.*}_cropped"
+ 	#ROI_LON_LAT="4.129657,1.668,79.123,80.598"
  	python crop_sentinel2_data.py \
  	    --output_file_format $format_file_out \
  	    --write_cropped_input_data \
- 	    --save_prefix $path_dir_out \
- 	    --roi_lon_lat=$roi_lon_lat \
- 	    $path_file_input \
- 	    $fname_file_out
+ 	    --save_prefix $PATH_DIR_OUT \
+ 	    --roi_lon_lat=$ROI_LON_LAT \
+ 	    $PATH_FILE_INPUT \
+ 	    $FNAME_FILE_OUT
  	#     # --write_images \
  	#     # --num_threads $num_threads \
  	#     #--list_bands \
