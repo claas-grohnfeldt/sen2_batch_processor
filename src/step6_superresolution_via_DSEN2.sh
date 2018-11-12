bc73d79c76a263f71992cca0d5ba72e882d7300f
claasg <claas.grohnfeldt@gmail.com>
2018-11-12 18:28:42 +0100

    updated

--- src/step6_superresolution_via_DSEN2.sh
+++ src/step6_superresolution_via_DSEN2.sh
@@ -15,7 +15,7 @@ for s2fname in $PATH_DIR_DATA_SEN2/*/*/tiles/S2*_MSIL1C_*.zip; do
     echo "s2dname = $s2dname"
     this_roi_lon_lat=$(cat $s2dname/../../ROI.csv)
     echo "this_rio_lon_lat = $this_roi_lon_lat"
-    python3 $PATH_FILE_PROG_DSEN2 $s2fname ${s2fname/.zip/_superres_test.tif} \
+    python3 $PATH_FILE_PROG_DSEN2 $s2fname ${s2fname/.SAFE.zip/_superres.tif} \
         --roi_lon_lat "$this_roi_lon_lat" # --roi_x_y "1000,1000,1200,1200" # 
         # --output_file_format "ENVI"
 done
