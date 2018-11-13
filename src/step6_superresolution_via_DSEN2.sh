80fbd40d97324198309f017b38f3b0e42b3e1fb7
claasg <claas.grohnfeldt@gmail.com>
2018-11-13 07:31:01 +0100

    changed to full image processing

--- src/step6_superresolution_via_DSEN2.sh
+++ src/step6_superresolution_via_DSEN2.sh
@@ -15,8 +15,9 @@ for s2fname in $PATH_DIR_DATA_SEN2/*/*/tiles/S2*_MSIL1C_*.zip; do
     echo "s2dname = $s2dname"
     this_roi_lon_lat=$(cat $s2dname/../../ROI.csv)
     echo "this_rio_lon_lat = $this_roi_lon_lat"
-    python3 $PATH_FILE_PROG_DSEN2 $s2fname ${s2fname/.SAFE.zip/_superres.tif} \
-        --roi_lon_lat "$this_roi_lon_lat" # --roi_x_y "1000,1000,1200,1200" # 
+    python3 $PATH_FILE_PROG_DSEN2 $s2fname ${s2fname/.SAFE.zip/_superres_full_tile.tif} \
+	--roi_x_y "0,0,12000,12000" # 
+        # --roi_lon_lat "$this_roi_lon_lat" # 
         # --output_file_format "ENVI"
 done
 
